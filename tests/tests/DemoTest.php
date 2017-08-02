f9181b622b59b65218b8f9f306818bbb83c22d72
Dennis Agulo <dennisagulo@gmail.com>
2017-08-02 08:52:43 +0800

    Test for willExpireAt() added

---
+++ tests/tests/DemoTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Unit;
+
+use Carbon\Carbon;
+use Tests\TestCase;
+
+class DemoTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function testWillExpireAt()
+    {
+        // set base time
+        $base_time = Carbon::createFromTimestamp( time() );
+
+        // test conditions here
+        // if difference is less than or equal to 24 hrs then output must be base time + 90 minutes
+        $test_time = Carbon::parse( $base_time )->addHour( );
+        $result = DTApi\Helpers\TeHelper::willExpireAt( $test_time, $base_time);
+        $this->assertEquals( $base_time->addMinutes( 90 ) , $result );
+        $test_time = Carbon::parse( $base_time )->addHour( 24 );
+        $result = DTApi\Helpers\TeHelper::willExpireAt( $test_time, $base_time);
+        $this->assertEquals( $base_time->addMinutes( 90 ) , $result );
+
+        // if difference is between 24 hrs and 72 hrs then output must be base time + 16 hours
+        $test_time = Carbon::parse( $base_time )->addHour( 25 );
+        $result = DTApi\Helpers\TeHelper::willExpireAt( $test_time, $base_time);
+        $this->assertEquals( $base_time->addHours( 16 ) , $result );
+        $test_time = Carbon::parse( $base_time )->addHour( 72 );
+        $result = DTApi\Helpers\TeHelper::willExpireAt( $test_time, $base_time);
+        $this->assertEquals( $base_time->addHours( 16 ) , $result );
+
+        // if difference is more than 72 hrs but less than or equal 90 hrs then output must be test time
+        $test_time = Carbon::parse( $base_time )->addHour( 73 );
+        $result = DTApi\Helpers\TeHelper::willExpireAt( $test_time, $base_time);
+        $this->assertEquals( $test_time , $result );
+        $test_time = Carbon::parse( $base_time )->addHour( 90 );
+        $result = DTApi\Helpers\TeHelper::willExpireAt( $test_time, $base_time);
+        $this->assertEquals( $test_time , $result );
+
+        //if test time is before base time the output must be in the past
+        $test_time = Carbon::parse( $base_time )->subHours( 24 );
+        $result = DTApi\Helpers\TeHelper::willExpireAt( $test_time, $base_time);
+        $this->assertEquals( $base_time->subHours( 16 ) , $result );
+
+    }
+
+}
